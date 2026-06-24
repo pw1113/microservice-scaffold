477736f43385a6cddc386a3829802db3ce3035bd
pw1113 <1773349257@qq.com>
2026-06-24 15:11:34 +0800

    1、user服务注册到nacos中 2、gateway服务nacos配置用户名和密码

--- microservice-user/microservice-user-service/src/main/java/com/microservice/user/service/MicroserviceUserServiceApplication.java
+++ microservice-user/microservice-user-service/src/main/java/com/microservice/user/service/MicroserviceUserServiceApplication.java
@@ -3,9 +3,11 @@ package com.microservice.user.service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.microservice.user.service", "com.microservice.common"})
 @MapperScan("com.microservice.user.service.mapper")
+@EnableDiscoveryClient
 public class MicroserviceUserServiceApplication {
 
 	public static void main(String[] args) {
